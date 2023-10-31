93d016b69276e12b89bd196a779d86490cc00c5a
Joachim Lombardi <lombardi.joachim@gmail.com>
2023-10-31 14:28:29 +0100

    debut

---
+++ Airplane.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from ydata_profiling import ProfileReport # conda install -c conda-forge ydata-profiling
+df = pd.read_csv('data/Airline_Dataset.csv')
+profile = ProfileReport(df,title="HCC Profiling Report")
+profile.to_notebook_iframe() 
+profile.to_file("report.html")
+# (fichier à ouvrir avec navigateur)
