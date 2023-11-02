2554778642f9a69211f56ec5980e63a8e1577604
pierre giannucci <pierregia13@gmail.com>
2023-11-02 08:51:21 +0100

    "modif app et formSatisfaction"

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 #dans app on appelle les différentes fonction des fichiers dans components
 
 import streamlit as st
+from components.formSatisfaction import formsatisfaction
 
-# Titre de l'application
-st.title("Application de Réservation de Vols")
+
+#import de la fonction affichant le formulaire de satisfaction
+formsatisfaction ()
+
 
---
+++ components/formSatisfaction.py
@@ -0,0 +1,8 @@
+
+#dans app on appelle les différentes fonction des fichiers dans components
+
+import streamlit as st
+
+def formsatisfaction ():
+    # Titre de l'application
+    st.title("Application de Réservation de Vols")
