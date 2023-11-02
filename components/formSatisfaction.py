fefeb2c7e5be65d011f7b9c6677d5e75c6270e8e
pierre giannucci <pierregia13@gmail.com>
2023-11-02 15:53:25 +0100

    "formsat"

--- components/formSatisfaction.py
+++ components/formSatisfaction.py
@@ -7,85 +7,77 @@ import pandas as pd
 
 
 def formsatisfaction():
-    
     '''
-    formulaire pour permettre a l'utilisateur de rentréer des paramètres et utiliser le model sur ces variables.
+    formulaire pour permettre à l'utilisateur de rentrer des paramètres et utiliser le modèle sur ces variables.
     '''
     # Titre de l'application
     st.title("Application de Vols")
 
     # Formulaire pour le genre
-    genre = st.radio("Genre:", ("Homme", "Femme"))
+    genre = st.selectbox("Genre:", ("Homme", "Femme"))
 
     # Formulaire pour le type de client loyal ou non 
-    type_client = st.radio("type de client:", ("Loyal", "Non loyal"))
+    type_client = st.selectbox("Type de client:", ("Loyal", "Non loyal"))
 
     # Champ pour l'âge
     age = st.number_input("Âge", min_value=0, max_value=150, value=25)
 
     # Formulaire pour le type de trajet
-    type_trajet = st.radio("type de trajet :", ("personel", "Business"))
+    type_trajet = st.selectbox("Type de trajet:", ("personnel", "Business"))
 
     # Formulaire pour la classe
-    classe = st.radio("type de vole :", ("Eco","Eco Plus", "Business"))
+    classe = st.selectbox("Type de vol:", ("Eco", "Eco Plus", "Business"))
 
-    # Champ pour l'âge
+    # Champ pour la distance
     distance = st.number_input("Distance", min_value=0, max_value=10000, value=300)
-    
-    # Formulaire pour le wifi
-    wifi = st.radio("Qualité du wifi :", ("0","1", "2","3","4","5"))
 
-    # Formulaire pour l'Heure de départ/arrivée pratique
-    heure_depart_arrivee = st.radio("Heure de départ/arrivée pratique :", ("0","1", "2","3","4","5"))
+    # Formulaire pour la qualité du wifi
+    wifi = st.selectbox("Qualité du wifi:", ("0", "1", "2", "3", "4", "5"))
 
-    
-    # Formulaire pour la Facilité de réservation en ligne
-    reservation = st.radio("Facilité de réservation en ligne :", ("0","1", "2","3","4","5"))
-    
+    # Formulaire pour l'heure de départ/arrivée pratique
+    heure_depart_arrivee = st.selectbox("Heure de départ/arrivée pratique:", ("0", "1", "2", "3", "4", "5"))
 
-    
-    # Formulaire pour l'Emplacement de la porte
-    emplacemement_porte = st.radio("Emplacement de la porte :", ("0","1", "2","3","4","5"))
-    
-    
-    # Formulaire pour la nourriture
-    food_drink = st.radio("nourriture:", ("0","1", "2","3","4","5"))
-    
-    #formulaire Embarquement en ligne
-    embarquement_ligne = st.radio("Embarquement en ligne:", ("0","1", "2","3","4","5"))
-    
-    #formulaire Confort d'assise
-    comfort = st.radio("Confort d'assise:", ("0","1", "2","3","4","5"))
-    
-    #Divertissement à bord
-    divertissement = st.radio("Divertissement à bord:", ("0","1", "2","3","4","5"))
-    
-    #formulaire Service à bord
-    service = st.radio("Service à bord:", ("0","1", "2","3","4","5"))
-    
-    #Service de chambre pour les jambes
-    service_jambes = st.radio("Service pour les jambes:", ("0","1", "2","3","4","5"))
-    
-    #formulaire pou La manutention des bagages
-    manutention_bagages = st.radio("manutention des bagages:", ("0","1", "2","3","4","5"))
-    
-    #formulaire Service d'enregistrement
-    service_enregistrement = st.radio("Service d'enregistrement:", ("0","1", "2","3","4","5"))
-    
-    #formulaire Service en vol
-    service_vole = st.radio("Service en vole:", ("0","1", "2","3","4","5"))
-    
-    #formulaire propreté
-    proprete = st.radio("Propreté:", ("0","1", "2","3","4","5"))
-    
-    
-    # Champ pour le retard du depart
+    # Formulaire pour la facilité de réservation en ligne
+    reservation = st.selectbox("Facilité de réservation en ligne:", ("0", "1", "2", "3", "4", "5"))
+
+    # Formulaire pour l'emplacement de la porte
+    emplacement_porte = st.selectbox("Emplacement de la porte:", ("0", "1", "2", "3", "4", "5"))
+
+    # Formulaire pour la nourriture/boissons
+    food_drink = st.selectbox("Nourriture/Boissons:", ("0", "1", "2", "3", "4", "5"))
+
+    # Formulaire pour l'embarquement en ligne
+    embarquement_ligne = st.selectbox("Embarquement en ligne:", ("0", "1", "2", "3", "4", "5"))
+
+    # Formulaire pour le confort d'assise
+    comfort = st.selectbox("Confort d'assise:", ("0", "1", "2", "3", "4", "5"))
+
+    # Formulaire pour le divertissement à bord
+    divertissement = st.selectbox("Divertissement à bord:", ("0", "1", "2", "3", "4", "5"))
+
+    # Formulaire pour le service à bord
+    service = st.selectbox("Service à bord:", ("0", "1", "2", "3", "4", "5"))
+
+    # Formulaire pour le service pour les jambes
+    service_jambes = st.selectbox("Service pour les jambes:", ("0", "1", "2", "3", "4", "5"))
+
+    # Formulaire pour la manutention des bagages
+    manutention_bagages = st.selectbox("Manutention des bagages:", ("0", "1", "2", "3", "4", "5"))
+
+    # Formulaire pour le service d'enregistrement
+    service_enregistrement = st.selectbox("Service d'enregistrement:", ("0", "1", "2", "3", "4", "5"))
+
+    # Formulaire pour le service en vol
+    service_vole = st.selectbox("Service en vol:", ("0", "1", "2", "3", "4", "5"))
+
+    # Formulaire pour la propreté
+    proprete = st.selectbox("Propreté:", ("0", "1", "2", "3", "4", "5"))
+
+    # Champ pour le retard du départ
     retard_depart = st.number_input("Retard de départ:", min_value=0, max_value=1900, value=60)
 
     # Champ pour le retard d'arrivée
-    retard_arrivee = st.number_input("Retard d'arrivée':", min_value=0, max_value=1900, value=60)
-
-
+    retard_arrivee = st.number_input("Retard d'arrivée:", min_value=0, max_value=1900, value=60)
 
     # Bouton pour soumettre le formulaire
     if st.button("Soumettre"):
@@ -100,7 +92,7 @@ def formsatisfaction():
             'Qualité du wifi': [wifi],
             'Heure de départ/arrivée pratique': [heure_depart_arrivee],
             'Facilité de réservation en ligne': [reservation],
-            'Emplacement de la porte': [emplacemement_porte],
+            'Emplacement de la porte': [emplacement_porte],
             'Nourriture/Boissons': [food_drink],
             'Embarquement en ligne': [embarquement_ligne],
             'Confort d\'assise': [comfort],
@@ -120,6 +112,7 @@ def formsatisfaction():
 
         # Affichez le DataFrame
         st.write("Données du formulaire:", df)
+
 
 
         #st.title("resultats")
