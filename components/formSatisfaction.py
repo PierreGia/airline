1192868f285b309fc7b485321510e92c6bf2b181
pierre giannucci <pierregia13@gmail.com>
2023-11-02 10:02:37 +0100

    "modif formulaire"

--- components/formSatisfaction.py
+++ components/formSatisfaction.py
@@ -3,6 +3,21 @@
 
 import streamlit as st
 
-def formsatisfaction ():
+def formsatisfaction():
     # Titre de l'application
-    st.title("Application de Réservation de Vols")
+    st.title("Application de Vols")
+
+    # Formulaire pour le genre
+    genre = st.radio("Genre:", ("Homme", "Femme"))
+
+    # Formulaire pour le type de client loyal ou non 
+    type = st.radio("type de client:", ("Loyal", "Non loyal"))
+
+    # Afficher le genre et le type de client sélectionnés
+    if genre:
+        st.write(f"Genre: {genre}")
+
+    if type:
+        st.write(f"Type de client: {type}")
+    
+   
