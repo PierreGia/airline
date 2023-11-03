533bf4ce8ece8ef40570cbf44154f3cef80467bf
Joachim Lombardi <lombardi.joachim@gmail.com>
2023-11-03 10:07:45 +0100

    "bdd"

--- components/formSatisfaction.py
+++ components/formSatisfaction.py
@@ -34,46 +34,46 @@ def formsatisfaction():
     distance = st.number_input("Flight Distance", min_value=0, max_value=10000, value=300)
     
     # Formulaire pour le wifi
-    wifi = st.radio("Inflight wifi service :", ("0","1", "2","3","4","5"))
+    wifi = st.radio("Inflight wifi service :", ("1","2","3","4","5"))
 
     # Formulaire pour l'Heure de départ/arrivée pratique
-    heure_depart_arrivee = st.radio("Departure/Arrival time convenient :", ("0","1", "2","3","4","5"))
+    heure_depart_arrivee = st.radio("Departure/Arrival time convenient :", ("1","2","3","4","5"))
 
     # Formulaire pour la Facilité de réservation en ligne
-    reservation = st.radio("Ease of Online booking :", ("0","1", "2","3","4","5"))
+    reservation = st.radio("Ease of Online booking :", ("1","2","3","4","5"))
     
     # Formulaire pour l'Emplacement de la porte
-    emplacemement_porte = st.radio("Gate location:", ("0","1", "2","3","4","5"))
+    emplacemement_porte = st.radio("Gate location:", ("1","2","3","4","5"))
     
     # # Formulaire pour la nourriture
     # food_drink = st.radio("nourriture:", ("0","1", "2","3","4","5"))
     
     #formulaire Embarquement en ligne
-    embarquement_ligne = st.radio("Online boarding:", ("0","1", "2","3","4","5"))
+    embarquement_ligne = st.radio("Online boarding:", ("1","2","3","4","5"))
     
     #formulaire Confort d'assise
-    comfort = st.radio("Seat comfort:", ("0","1", "2","3","4","5"))
+    comfort = st.radio("Seat comfort:", ("1","2","3","4","5"))
     
     # #Divertissement à bord
     # divertissement = st.radio("Divertissement à bord:", ("0","1", "2","3","4","5"))
     
     #formulaire Service à bord
-    service = st.radio("On-board service:", ("0","1", "2","3","4","5"))
+    service = st.radio("On-board service:", ("1","2","3","4","5"))
     
     # #Service de chambre pour les jambes
     # service_jambes = st.radio("Service pour les jambes:", ("0","1", "2","3","4","5"))
     
     #formulaire pou La manutention des bagages
-    manutention_bagages = st.radio("Baggage handling:", ("0","1", "2","3","4","5"))
+    manutention_bagages = st.radio("Baggage handling:", ("1","2","3","4","5"))
     
     #formulaire Service d'enregistrement
-    service_enregistrement = st.radio("Checkin service:", ("0","1", "2","3","4","5"))
+    service_enregistrement = st.radio("Checkin service:", ("1","2","3","4","5"))
     
     # #formulaire Service en vol
     # service_vole = st.radio("Inflight entertainment:", ("0","1", "2","3","4","5"))
     
     #formulaire propreté
-    proprete = st.radio("Cleanliness:", ("0","1", "2","3","4","5"))
+    proprete = st.radio("Cleanliness:", ("1","2","3","4","5"))
     
     # # Champ pour le retard du depart
     # retard_depart = st.number_input("Retard de départ:", min_value=0, max_value=1900, value=60)
